806ec7a9f399bc43b96dbca70c5f937a1520b8bc
System Administrator <root@ATLISs-MacBook-Pro.local>
2015-12-15 11:35:52 +0200

    Switch to 15.12.15-4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello 15.12.15-3');
+  res.send('hello 15.12.15-4');
 });
  
 app.listen(process.env.PORT || 5000);
